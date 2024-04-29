a81e606f47a5682c0b2e83a310e6ae8225af829d
Sungho <shson1217@kw.ac.kr>
2024-04-29 16:11:38 +0900

    feat: applyDiff알고리즘 적용

---
+++ chapter02/seoson/05/index.js
@@ -0,0 +1,30 @@
+import getTodos from "./getTodos.js";
+import todosView from "./view/todos.js";
+import counterView from "./view/counter.js";
+import filtersView from "./view/filters.js";
+import registry from "./registry.js";
+import applyDiff from "./applyDiff.js";
+
+registry.add("todos", todosView);
+registry.add("counter", counterView);
+registry.add("filters", filtersView);
+
+const state = {
+  todos: getTodos(),
+  currentFilter: "All",
+};
+
+const render = () => {
+  window.requestAnimationFrame(() => {
+    const $main = document.querySelector(".todoapp");
+    const newMain = registry.renderRoot($main, state);
+    applyDiff(document.body, $main, newMain);
+  });
+};
+
+window.setInterval(() => {
+  state.todos = getTodos();
+  render();
+}, 5000);
+
+render();
---
+++ chapter02/seoson/05/view/filters.js
@@ -0,0 +1,12 @@
+export default (targetElement, { currentFilter }) => {
+  const newCounter = targetElement.cloneNode(true);
+
+  Array.from(newCounter.querySelector("li a")).forEach((a) => {
+    if (a.textContent === currentFilter) {
+      a.classList.add("selected");
+    } else {
+      a.classList.remove("selected");
+    }
+  });
+  return newCounter;
+};
